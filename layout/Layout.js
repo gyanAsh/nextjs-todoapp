eb3b2ea4d31ad645a505688342ce12f9bd8da339
gyanAsh <gyanash36@protonmail.com>
2022-08-01 23:20:02 +0530

    Layout Component Updated

--- layout/Layout.js
+++ layout/Layout.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const Layout = ({children}) => {
-  return (
-      <div>
-          <Header/>
-          {children}
-          <Footer/>
-    </div>
-  )
+const Layout = (props) => {
+    const { children } = props;
+    return (
+        <div className='flex flex-col min-h-screen relative bg-slate-900 text-white'>
+            <Header />
+            <main className='flex-1 flex flex-col p-4'>
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
 }
-
-export default Layout
+export default Layout;
